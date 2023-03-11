f5efad92e8ae4ea000afdaa603bbfc56869cde3b
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-03-11 15:21:30 -0600

    Clean up vite config

--- annotation-ui/vite.config.ts
+++ annotation-ui/vite.config.ts
@@ -1,20 +1,13 @@
-/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: "happy-dom",
-    deps: {
-      inline: ["@react-spectrum"],
-    },
-  },
   server: {
     proxy: {
       "/api/v1": {
-        target: "http://localhost:8080",
+        target: "http://localhost:8000",
         rewrite: (path) => {
           return path.replace("/api/v1", "");
         },
