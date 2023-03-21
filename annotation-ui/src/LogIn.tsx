cb916f94cc78545bc1855af4393aae44b5858f8f
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-03-21 09:29:11 -0500

    Add weeks dropdown to UI

--- annotation-ui/src/LogIn.tsx
+++ annotation-ui/src/LogIn.tsx
@@ -3,12 +3,20 @@ import { Flex, Heading, Button, Picker, Item } from "@adobe/react-spectrum";
 import { useLocation } from "wouter";
 import assignments from "./assignments.json";
 
+const WEEKS = {
+  MARCH_13: "March 13th",
+  MARCH_20: "March 20th",
+};
+
+type Week = keyof typeof WEEKS | "";
+
 export const LogIn = () => {
   const [name, setName] = React.useState("");
+  const [week, setWeek] = React.useState<Week>("");
   const [_, setLocation] = useLocation();
   const gotoApp = React.useCallback(() => {
-    setLocation(`/${name}`);
-  }, [name, setLocation]);
+    setLocation(`/${name}?week=${week}`);
+  }, [name, setLocation, week]);
   return (
     <Flex
       direction="column"
@@ -19,10 +27,12 @@ export const LogIn = () => {
     >
       <Flex direction="column">
         <Flex>
-          <Heading level={1}>Enter Your Name</Heading>
+          <Heading level={2}>Welcome</Heading>
         </Flex>
         <Flex marginBottom="32px">
           <Picker
+            selectedKey={name}
+            placeholder="Name..."
             onSelectionChange={(key) => {
               setName(key as string);
             }}
@@ -32,10 +42,23 @@ export const LogIn = () => {
             })}
           </Picker>
         </Flex>
+        <Flex marginBottom="32px">
+          <Picker
+            selectedKey={week}
+            placeholder="Week..."
+            onSelectionChange={(key) => {
+              setWeek(key as Week);
+            }}
+          >
+            {Object.entries(WEEKS).map(([key, display]) => {
+              return <Item key={key}>{display}</Item>;
+            })}
+          </Picker>
+        </Flex>
         <Flex marginBottom="16px">
           <Button
             data-testid="GOTO_APP"
-            isDisabled={name === ""}
+            isDisabled={name === "" || week === ""}
             variant="primary"
             onPress={gotoApp}
           >
