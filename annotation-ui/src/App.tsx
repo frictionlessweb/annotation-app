b64c93f56d93e440fb8ef6dfb008d079916b8f87
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-03-22 16:49:46 -0500

    Background color white

--- annotation-ui/src/App.tsx
+++ annotation-ui/src/App.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { Provider, defaultTheme } from "@adobe/react-spectrum";
 import { ToastContainer } from "@react-spectrum/toast";
 import { Router } from "./Router";
 
 export const App = () => {
   return (
-    <Provider theme={defaultTheme} colorScheme="light" height="100%">
+    <Provider theme={defaultTheme} colorScheme="light" height="100%" UNSAFE_style={{ backgroundColor: 'white' }}>
       <Router />
       <ToastContainer />
     </Provider>
