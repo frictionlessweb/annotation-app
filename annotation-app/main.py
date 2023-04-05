09143e6023ab6289d781ce1c8b31840e768d5844
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-04-05 11:12:14 -0500

    Cleanup API

--- annotation-app/main.py
+++ annotation-app/main.py
@@ -7,7 +7,6 @@ from database import SessionLocal
 from sqlalchemy.orm import Session
 from models import Sessions
 from datetime import datetime
-from itertools import groupby
 
 app = FastAPI(root_path="/api/v1")
 app.mount("/static", StaticFiles(directory="assets"), name="static")
@@ -16,11 +15,6 @@ app.mount("/static", StaticFiles(directory="assets"), name="static")
 @app.get("/heartbeat")
 def read_root():
     return {"message": "success"}
-
-
-@app.get("/documents")
-def another_route(week: str = "") -> dict[str, Any]:
-    return DOCUMENT_MAP.get(week, DOCUMENT_MAP["MARCH_13"])
 
 
 class SessionState(BaseModel):
@@ -34,132 +28,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-def march_13_complete(document_map):
-    """
-    Determine if the document is finished or not.
-    """
-    for document in document_map:
-        topics = document_map[document]
-        if not isinstance(topics, dict):
-            continue
-        for topic_id in topics:
-            annotations = topics[topic_id]
-            if not isinstance(annotations, dict):
-                continue
-            for annotation_id in annotations:
-                if not isinstance(annotations[annotation_id], bool):
-                    return False
-    return True
-
-
-@app.get("/responses")
-def current_database(day: str, db: Session = Depends(get_db)):
-    time: datetime
-    try:
-        time = datetime.strptime(day, "%m-%d-%Y")
-    except ValueError:
-        return []
-    greater_than = f"{time.year}-{time.month}-{time.day - 1}"
-    less_than = f"{time.year}-{time.month}-{time.day + 1}"
-    results = db.query(Sessions).filter(
-        Sessions.created_at < less_than, Sessions.created_at > greater_than
-    )
-    return results.all()
-
-
-def is_march_13(document_map) -> bool:
-    for doc_id in document_map:
-        a_map = document_map[doc_id]
-        if not isinstance(a_map, dict):
-            return False
-    return True
-
-
-def complete_record(a_map) -> bool:
-    return all(val == True or val == False for val in a_map["answers"].values())
-
-
-def qa_complete(a_map) -> bool:
-    return all(val["visited"] for val in a_map["answers"].values())
-
-
-def annotation_week(document_map) -> str | None:
-    for doc_id in document_map:
-        a_map = document_map[doc_id]
-        if not isinstance(a_map, dict):
-            return None
-        return "MARCH_20" if "questionTask" in a_map else "MARCH_13"
-
-
-def march_20_complete(document_map) -> bool:
-    for doc_id in document_map:
-        document = document_map[doc_id]
-
-        question_task_complete = complete_record(document["questionTask"])
-        if not question_task_complete:
-            return False
-
-        statement_task_complete = complete_record(document["statementsTask"])
-        if not statement_task_complete:
-            return False
-
-        topic_task_complete = complete_record(document["topicTask"])
-        if not topic_task_complete:
-            return False
-
-        qa_task_complete = qa_complete(document["qaTask"])
-        if not qa_task_complete:
-            return False
-
-    return True
-
-
-@app.get("/response-by-person")
-def user_responses(name: str, db: Session = Depends(get_db)):
-    buf = []
-    responses = db.query(Sessions).filter(Sessions.user_name == name).all()
-    for response in responses:
-        week = annotation_week(response.annotations)
-        if week == "MARCH_13":
-            buf.append(
-                {
-                    "complete": march_13_complete(response.annotations),
-                    "json": response.annotations,
-                    "week": "March 13th",
-                    "created_at": response.created_at,
-                }
-            )
-        elif week == "MARCH_20":
-            buf.append(
-                {
-                    "complete": march_20_complete(response.annotations),
-                    "json": response.annotations,
-                    "week": "March 20th",
-                    "created_at": response.created_at,
-                }
-            )
-    output = []
-    groups = groupby(buf, lambda x: x["week"])
-    for _, group in groups:
-        ordered = sorted(group, key=lambda x: x["created_at"], reverse=True)
-        output.append(ordered[0])
-    return output
-
-@app.get("/app-responses")
-def all_responses():
-    return Session.query.all()
-
-
-@app.post("/save-session")
-def save_session(state: SessionState, db: Session = Depends(get_db)):
-    db_annotation = Sessions(
-        user_name=state.user_name,
-        annotations=state.annotations,
-        created_at=datetime.now(),
-        updated_at=datetime.now(),
-    )
-    db.add(db_annotation)
-    db.commit()
-    return {"message": "success"}
--- annotation-app/test_main.py
+++ annotation-app/test_main.py
@@ -1,45 +1,8 @@
 from fastapi.testclient import TestClient
-from main import app, complete
+from main import app
 
 
 client = TestClient(app)
-
-
-def test_complete():
-    assert complete({"document": {"topic": {"annotation": True}}})
-    assert complete(
-        {
-            "document": {
-                "topic": {
-                    "annotation": True,
-                    "another_annotation": True,
-                    "a_third_annotation": True,
-                },
-                "topic_two": {
-                    "a_fourth_annotation": True,
-                    "a_fifth_annotation": True,
-                    "a_sixth_annotation": False,
-                },
-            }
-        }
-    )
-    assert not complete({"document": {"topic": {"annotation": None}}})
-    assert not complete(
-        {
-            "document": {
-                "topic": {
-                    "annotation": True,
-                    "another_annotation": True,
-                    "a_third_annotation": True,
-                },
-                "topic_two": {
-                    "a_fourth_annotation": True,
-                    "a_fifth_annotation": None,
-                    "a_sixth_annotation": False,
-                },
-            }
-        }
-    )
 
 
 def test_heartbeat():
@@ -52,30 +15,3 @@ def test_documents():
     response = client.get("/documents")
     assert response.status_code == 200
     assert response.json() is not None
-
-
-def test_static_files():
-    response = client.get("/documents")
-    assert response.status_code == 200
-    response_json = response.json()
-    document_names: list[str] = [key for key in response_json]
-    for name in document_names:
-        expected_json = client.get(f"/static/{name}.json")
-        assert expected_json.status_code == 200
-        # Our API is mounted at /api/v1, so we need to replace this in the URL,
-        # but the front-end can just read the key off the JSON without worrying.
-        pdf_url = response_json[name]["pdf_url"].replace("/api/v1", "")
-        expected_pdf = client.get(pdf_url)
-        assert expected_pdf.status_code == 200
-
-
-def test_get_database():
-    response = client.get("/responses?day=03-17-2023")
-    assert response.status_code == 200
-
-
-def test_save_items():
-    response = client.post(
-        "/save-session", json={"user_name": "Jill", "annotations": {"key": True}}
-    )
-    assert response.status_code == 200
