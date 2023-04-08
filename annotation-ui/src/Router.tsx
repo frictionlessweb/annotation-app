4cc43c9becc02e59d2898d3e8f69ad48ddce5652
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-04-07 20:35:47 -0500

    More tweaks

--- annotation-ui/src/Router.tsx
+++ annotation-ui/src/Router.tsx
@@ -1,13 +1,38 @@
 import React from "react";
 import { Flex, Text } from "@adobe/react-spectrum";
-import { Route } from "wouter";
-import { DocumentRouter } from './DocumentRouter';
+import { Route, Link } from "wouter";
+import { DocumentRouter } from "./DocumentRouter";
+
+const DOCUMENTS = [
+  "E0CEG1S47",
+  "E0CMG4S162",
+  "F100",
+  "F46",
+  "handbook_estate_planning_F132",
+  "proposal_sales_proposal_F49",
+  "agreement_professional_disclosure_statement_DR--256324984",
+  "instructions_product_owner_manual_DR--338379095",
+  "report_IBM_Whitepaper_on_AI_&_Business",
+  "report_financial_spotify_report",
+];
 
 export const Router = () => {
   return (
     <Flex direction="column">
       <Route path="/">
-        <Text>Welcome.</Text>
+        <Flex direction="column" margin="8px">
+          <Text>Welcome!</Text>
+          <Text>Please select a task:</Text>
+          <ul>
+            {DOCUMENTS.map((doc) => {
+              return (
+                <li key={doc}>
+                  <Link to={`/${doc}`}>{doc}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </Flex>
       </Route>
       <Route path="/:document">
         <DocumentRouter />
